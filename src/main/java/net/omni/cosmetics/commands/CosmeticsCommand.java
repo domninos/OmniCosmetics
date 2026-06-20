4b50d65e727fc13cd24d3ee149e86de7f808255a
domninos <domninosdev@gmail.com>
2026-06-20 18:13:00 +0800

    feat: implemented commands

--- src/main/java/net/omni/cosmetics/commands/CosmeticsCommand.java
+++ src/main/java/net/omni/cosmetics/commands/CosmeticsCommand.java
@@ -1,8 +1,10 @@
 package net.omni.cosmetics.commands;
 
 import net.omni.cosmetics.OmniCosmetics;
+import net.omni.cosmetics.util.config.Messages;
 import org.bukkit.Bukkit;
 import org.bukkit.command.*;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,23 +19,86 @@ public class CosmeticsCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        return false;
+        if (args.length == 0) {
+            if (!(sender instanceof Player)) {
+                plugin.sendMessage(sender, Messages.ONLY_PLAYERS.toString());
+                return true;
+            }
+
+            if (!sender.hasPermission("omnicosmetics.menu")) {
+                plugin.sendMessage(sender, Messages.NO_PERMS.toString());
+                return true;
+            }
+
+            plugin.getGuiManager().openMenu((Player) sender);
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("reload")) {
+            if (!sender.hasPermission("omnicosmetics.reload")) {
+                plugin.sendMessage(sender, Messages.NO_PERMS.toString());
+                return true;
+            }
+
+            plugin.reload();
+            plugin.sendMessage(sender, Messages.RELOADED.toString());
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("benchmark")) {
+            if (!sender.hasPermission("omnicosmetics.benchmark")) {
+                plugin.sendMessage(sender, Messages.NO_PERMS.toString());
+                return true;
+            }
+            if (!(sender instanceof Player player)) {
+                plugin.sendMessage(sender, Messages.ONLY_PLAYERS.toString());
+                return true;
+            }
+
+            if (plugin.getBenchmarkManager().isBenchmarking(player)) {
+                plugin.getBenchmarkManager().stopBenchmark(player.getUniqueId());
+                plugin.sendMessage(player, "<gray>Benchmark mode <red>disabled</red>.</gray>");
+            } else {
+                int radius = 1;
+                if (args.length >= 2) {
+                    try {
+                        radius = Integer.parseInt(args[1]);
+                    } catch (NumberFormatException ignored) {
+                    }
+                }
+                plugin.getBenchmarkManager().startBenchmark(player, radius);
+                plugin.sendMessage(player, "<gray>Benchmark mode <green>enabled</green> (radius=" + radius + ").</gray>");
+            }
+            return true;
+        }
+
+        plugin.sendMessage(sender, Messages.USAGE.replace("usage", "/cosmetics [reload|benchmark]"));
+        return true;
     }
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        if (args.length == 1) {
+            List<String> options = new java.util.ArrayList<>();
+            if (sender.hasPermission("omnicosmetics.reload"))
+                options.add("reload");
+            if (sender.hasPermission("omnicosmetics.benchmark"))
+                options.add("benchmark");
+            return options;
+        }
+
         return List.of();
     }
 
     public void register() {
-        PluginCommand cosmetics = Bukkit.getServer().getPluginCommand("cosmetics");
+        PluginCommand cmd = Bukkit.getServer().getPluginCommand("cosmetics");
 
-        if (cosmetics == null) {
+        if (cmd == null) {
             plugin.getLogger().warning("/cosmetics command not found!");
             return;
         }
 
-        cosmetics.setTabCompleter(this);
-        cosmetics.setExecutor(this);
+        cmd.setTabCompleter(this);
+        cmd.setExecutor(this);
     }
 }
